ac8d54d5d50072418072ce89b6d5b7e6e4900576
Ayush-Rajcoomar <st10074013@vcconnect.edu.za>
2024-11-25 18:01:49 +0100

    AllViewsDone

--- StudentAttendanceWebApp/obj/Debug/net8.0/StudentAttendanceWebApp.AssemblyInfo.cs
+++ StudentAttendanceWebApp/obj/Debug/net8.0/StudentAttendanceWebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentAttendanceWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99f3629f3f5d59da3923aea1dc0043fd8794d7f2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2af504a0636a0339e291cd6912f1876bb1fdaf6d")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentAttendanceWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentAttendanceWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
